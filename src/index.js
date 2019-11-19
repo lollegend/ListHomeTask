16f4659aad1050e831f8e5027561d376857822dd
lollegend <terenevichlollegend@gmail.com>
2019-11-19 20:22:18 +0300

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -17,24 +17,35 @@ const elementSize = 20;
 
 function updateListAfterKeypress() {
     [...document.querySelectorAll(".item")].forEach(element => element.remove());
-    itemLoading(0,29, filterList(searchInput.value, data), listNode);
+    itemLoading(0, 29, filterList(searchInput.value, data), listNode);
 }
 
 searchInput.addEventListener("keydown", updateListAfterKeypress);
+searchInput.addEventListener("keydown", scrollAnchorReset);
+
 
 let scrollAnchor = document.querySelector(".list-app").scrollTop
     - document.querySelector(".list-app").offsetHeight;
+
+function scrollAnchorReset() {
+    document.querySelector(".list-app").scrollTop = 0;
+    scrollAnchor = document.querySelector(".list-app").scrollTop
+        - document.querySelector(".list-app").offsetHeight;
+}
 
 document.querySelector(".list-app").addEventListener('scroll', e => {
     const { scrollTop } = e.target;
     if (scrollAnchor < scrollTop - elementSize * (loadStep - 2)) {
         scrollAnchor = scrollTop;
         itemLoading([...document.querySelectorAll(".item")].length
-            ,loadStep
+            , loadStep
             , filterList(searchInput.value, data)
             , listNode);
     }
 });
 
+searchInput.addEventListener("keydown", updateListAfterKeypress);
+searchInput.addEventListener("keydown", scrollAnchorReset);
+
 //first load
-itemLoading(0,29, filterList(searchInput.value, data), listNode);
+itemLoading(0, 29, filterList(searchInput.value, data), listNode);
